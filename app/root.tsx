45649b355e434464630086dda4889c530f42bde9
Saharsh <saharshg10@gmail.com>
2024-01-17 14:10:43 +0530

    Selected contact style

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -69,7 +70,12 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? 'active' : isPending ? 'pending' : ''
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -78,7 +84,7 @@ export default function App() {
                         <i>No Name</i>
                       )}{' '}
                       {contact.favorite ? <span>★</span> : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
