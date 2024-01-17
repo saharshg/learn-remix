101b817fdaeef2459786c00a1218621b3d755c1c
Saharsh <saharshg10@gmail.com>
2024-01-17 14:05:22 +0530

    Editing a contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { json } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
 
 import {
   Form,
@@ -23,7 +23,7 @@ import appStylesHref from './app.css';
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact });
+  return redirect(`/contacts/${contact.id}/edit`);
 };
 
 export const loader = async () => {
